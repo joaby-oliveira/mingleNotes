7f150caf998caa2e1244317e6ac01b3451c3f577
Joaby <joabyoliveira2004@gmail.com>
2021-03-18 19:09:29 -0300

    chore: add and set .env

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -1,9 +1,11 @@
+require('dotenv/').config();
+
 const knex = require('knex')({
   client: 'mysql2',
   connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: 'mingleNotes'
   }
 });
