ec16e20e3fef91321b35c27ccadc8f2d5c4ba18b
Joaby <joabyoliveira2004@gmail.com>
2021-03-17 22:35:33 -0300

    feat: set routes configuration

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Router = require('./routes'); 
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(Routes)
+
+app.use(Router);
 
 
 app.listen(3000, ()=>{
