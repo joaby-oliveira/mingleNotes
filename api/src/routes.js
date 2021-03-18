de007b6a096212369380eccfd86b1bd96fbdb247
Joaby <joabyoliveira2004@gmail.com>
2021-03-18 19:10:32 -0300

    feat: add create Tables route

--- api/src/routes.js
+++ api/src/routes.js
@@ -4,11 +4,16 @@ const app = express();
 const router = express.Router();
 
 const UserController = require("./controllers/UserController");
+const TableController = require('./controllers/TableController');
 
 router.get('/', (req, res) => {
     res.send('eaeee');
 });
 
 router.post("/user", UserController.create);
+
+
+router.post("/table/:userId", TableController.create);
+
 
 module.exports = router;
