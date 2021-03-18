b86aab38d884512689b9db707109dbbe273007c8
Joaby <joabyoliveira2004@gmail.com>
2021-03-17 22:02:05 -0300

    fix: remove files from wrong directory

--- api/src/server.js
+++ api/src/server.js
@@ -3,4 +3,9 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(3000, ()=>{
+    console.log('Server is running on http://localhost:3000');
+});
