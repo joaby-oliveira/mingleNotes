b709d7d1a00b727c9dadefe028c25b49587da89a
Joaby <joabyoliveira2004@gmail.com>
2021-03-18 19:14:51 -0300

    feat: create function to validate id

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -8,6 +8,21 @@ class User{
             console.log(err);
         }
     }
+
+    async validateId (id) {
+        try{
+            const idExists = await database.select().from('users').where('id', id);
+            console.log(idExists);
+            if (idExists[0]) {
+                return {status: true};
+            } else {
+                return false;
+            }
+        }catch(err) {
+            console.log(err);
+        }
+    } 
+
 }
 
 module.exports = new User();
