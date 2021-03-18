9fb3aa91175744f548a91f871c1ad71ce3f0147b
Joaby <joabyoliveira2004@gmail.com>
2021-03-17 22:18:53 -0300

    feat: set bodyparser

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(Routes)
+
 
 app.listen(3000, ()=>{
     console.log('Server is running on http://localhost:3000');
