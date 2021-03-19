994c7fd9154782eebd8549403842201669638ab4
Joaby <joabyoliveira2004@gmail.com>
2021-03-19 19:33:11 -0300

    feat: add indentation & get tables method

--- api/src/controllers/TableController.js
+++ api/src/controllers/TableController.js
@@ -13,19 +13,19 @@ class TableController {
                 data.name = name;
             } else {
                 res.statusCode = 406;
-                res.json({status: false, msg: 'Campo de nome não preenchido' });
+                res.json({ status: false, msg: 'Campo de nome não preenchido' });
             }
             if (imgUrl != undefined) {
                 data.imgUrl = imgUrl;
             } else {
                 res.statusCode = 406;
-                res.json({status: false, msg: 'Preencha o campo referência corretamente' });
+                res.json({ status: false, msg: 'Preencha o campo referência corretamente' });
             }
             if (userId != undefined) {
                 data.user_id = userId;
             } else {
                 res.statusCode = 406;
-                res.json({status: false, msg: 'É necessário informar o id do usuário a quem pertence essa tabela' });
+                res.json({ status: false, msg: 'É necessário informar o id do usuário a quem pertence essa tabela' });
             }
 
             const userIdExists = await User.validateId(userId);
@@ -36,18 +36,48 @@ class TableController {
                     res.statusCode = 200;
                     res.json({ status: true, msg });
                 } else {
-                    res.statusCode = 406;
+                    res.statusCode = 201;
                     res.json({ status: false, msg: 'a' })
                 }
             } else {
-                res.statusCode = 406;
+                res.statusCode = 404;
                 res.json({ status: false, msg: 'Id de usuário informado não existe' })
             }
         } catch (err) {
             res.statusCode = 406;
-            res.json({ status: false, msg: 'Algum erro aconteceu'})
+            res.json({ status: false, msg: 'Algum erro aconteceu' })
         }
     }
+
+    async getAll(req, res) {
+        try {
+            const {userId} = req.body; 
+            if(userId != undefined) {
+                const userIdExists = await User.validateId(userId);
+                
+                if(userIdExists) {
+                    const { status, code, data } = await Table.getAll(userId);
+                    console.log(data);
+                    if (status) {
+                        res.statusCode = code;
+                        res.json(data);
+                    } else {
+                        res.json(data);
+                    }
+                }else {
+                    res.statusCode = 404;
+                    res.json({msg: "Id de usuário informado não existe"});
+                }
+            }else {
+                res.statusCode = 400;
+                res.json({msg: "Id de usuário não informado"});
+            }
+        }catch (err) {
+            res.statusCode = 400;
+            res.json({msg: err});
+        }
+    }
+
 }
 
 module.exports = new TableController();
