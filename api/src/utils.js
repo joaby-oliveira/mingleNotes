8d010e7c37ac9388c126466cdcbd9eea33fea928
Joaby <joabyoliveira2004@gmail.com>
2021-03-20 20:29:45 -0300

    feat: create our own validations module

---
+++ api/src/utils.js
@@ -0,0 +1,11 @@
+const utils = {
+    isEmpty: (str)=>{
+        if(str != '') {
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
+
+module.exports = utils;
